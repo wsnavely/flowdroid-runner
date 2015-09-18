c72bbb59d6818d33feb50fe90815ca3f418b1154
Will Snavely <wsnavely@sei.cmu.edu>
2015-09-18 22:58:13 +0100

    Adding s3 publishing

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import os
 import os.path
 import logging
 import errno
+import argparse
 
 def mkdir_p(path):
     try:
@@ -19,6 +20,14 @@ platforms = "/home/ubuntu/platforms"
 reporoot = "/home/ubuntu/flowdroid-runner"
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j", "--jvmargs")
+    parser.add_argument("-o", "--s3out")
+    args = parser.parse_args()
+
+    jvm_args = args.jvmargs
+    s3_out = args.s3out
+    
     logging.basicConfig(level="DEBUG")
     log_dir = os.path.join(outdir, "log")
     flowdroid_dir = os.path.join(outdir, "flowdroid")
@@ -34,7 +43,7 @@ if __name__ == "__main__":
         fd_out = os.path.join(flowdroid_dir, (apk_name + ".xml"))
 
         logging.info("Processing APK: " + apk_path)
-        args = sys.argv[1:]
+        args = jvm_args
         args += ["-jar", os.path.join(reporoot, "didfail_phase1.jar")]
         args += ["-apk", apk_path]
         args += ["-platforms", platforms]
@@ -42,6 +51,9 @@ if __name__ == "__main__":
         args += ["-config", os.path.join(reporoot, "didfail.json")]
         args += ["-ss", os.path.join(reporoot, "SourcesAndSinks.txt")]
         args += ["-tw", os.path.join(reporoot, "EasyTaintWrapperSource.txt")]
-
+        
         cmd = command.Command("/usr/bin/java", args, stdout=fd_log, stderr=fd_log)
 	cmd.run(60*15)
+
+    s3_cmd = "s3cmd sync " + outdir + " " + s3_out
+    os.system(s3_cmd)    
